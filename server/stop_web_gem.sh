e8ad92294d111475a04c685db8f0c8392e48b2b2
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2025-10-23 21:48:28 +0200

    fix start/stop gem script

--- server/stop_web_gem.sh
+++ server/stop_web_gem.sh
@@ -3,6 +3,8 @@
 STONE=$1
 PORT=$2
 
+cd $(dirname "${BASH_SOURCE[0]}")
+
 # $GS_HOME/bin/todeIt $STONE /home/seaside/gemServer --stop=seaside$PORT
 
 ./executeSmalltalk.sh $STONE "(GemServer gemServerNamed: 'seaside$PORT') stopGems"
